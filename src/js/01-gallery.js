1f8846d08e8bc06558fe9f0f9358cf26e919961f
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-24 15:10:26 +0200

    task 1 npm lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+            return `<a class = "gallery__item"
+            href = "${original}">
+                <img class = "gallery__image"
+            src = "${preview}"
+            alt = "${description}" />
+                </a>`;
+        })
+        .join('');
+};
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: `alt`,
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
