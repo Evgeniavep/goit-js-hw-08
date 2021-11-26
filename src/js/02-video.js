7c694164fc45b2de2d572d0030e9a9c7e0f22984
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-26 15:19:23 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Проверка на наличие записей в localStorage//
+
+function setTime() {
+    const isTimeSaved = localStorage.getItem("videoplayer-current-time");
+    if (isTimeSaved) {
+        player.setCurrentTime(JSON.parse(isTimeSaved));
+    }
+}
+
+window.addEventListener('DOMContentLoaded', setTime)
+    // player.on('play', onPlay)
+
+// Сохранение данных в localStorage//
+
+player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", data.seconds), 1000));
