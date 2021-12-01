1c0358dded4da9e94a66511086d76798fa42ac02
Evhenia Vepretskaya <enychka12@gmail.com>
2021-12-01 15:57:10 +0200

    все работает по message

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const textArea = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
 
+form.addEventListener('submit', onFormSubmit);
+textArea.addEventListener('input', throttle(onTextAreaInput, 500));
+
+fillInTextarea();
+
+
+// Отправляем сообщение, очищаем форму, очищаем localStorage
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+
+// Сохраняем введенные данные в localStorage
+
+function onTextAreaInput(evt) {
+    const message = evt.target.value;
+
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+// Заполнить поле формы из localStorage
+
+function fillInTextarea(evt) {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        textArea.value = savedMessage
+    };
+}
